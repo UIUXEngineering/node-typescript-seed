a560224542fb0dd6b1e16a00cb8d8a89fb7e2179
Jerry Orta <jerryorta.dev@gmail.com>
2016-12-07 02:32:37 -0600

    fix(config): comment unused prop in config

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -17,10 +17,10 @@ export class ProjectConfig extends SeedConfig {
     // this.HOT_LOADER_PORT = 5578;
 
     // Add `local` third-party libraries to be injected/bundled.
-    this.APP_ASSETS = [
-      ...this.APP_ASSETS,
-
-    ];
+    // this.APP_ASSETS = [
+    //   ...this.APP_ASSETS,
+    //
+    // ];
 
     /* Add to or override NPM module configurations: */
     // this.mergeObject(this.PLUGIN_CONFIGS['browser-sync'], { ghostMode: false });
