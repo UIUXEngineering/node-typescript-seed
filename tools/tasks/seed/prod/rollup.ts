2f5d13e5bf6528027c11a1db81923946c689a55f
Jerry Orta <jerryorta.dev@gmail.com>
2016-12-09 07:32:40 -0600

    feat(umd): rollup umd bundling task

--- tools/tasks/seed/prod/rollup.ts
+++ tools/tasks/seed/prod/rollup.ts
@@ -4,6 +4,7 @@ import * as gulpLoadPlugins from 'gulp-load-plugins';
 import { join, sep  } from 'path';
 import { JSONParse } from '../../../utils';
 import * as merge from 'merge2';
+import * as fs from 'fs';
 import Config from '../../../config';
 
 const plugins = <any>gulpLoadPlugins();
@@ -19,27 +20,35 @@ export = (done: any) => {
 
   rollup({
     entry: rollupConfig.entry.split('/').join(sep),
+    sourceMap: true,
     cache: cache
   }).then( function ( bundle ) {
     // Generate bundle + sourcemap
     // var result = bundle.generate({
+    //   moduleName: 'typescript.seed',
     //   // output format - 'amd', 'cjs', 'es', 'iife', 'umd'
-    //   format: rollupConfig.format
+    //   format: 'umd'
     // });
+
+    // console.log(result);
 
     // Cache our bundle for later use (optional)
     cache = bundle;
-
-    console.log(bundle);
 
     // fs.writeFileSync( rollupConfig.dest.split('/').join(sep), result.code );
 
     // Alternatively, let Rollup do it for you
     // (this returns a promise). This is much
     // easier if you're generating a sourcemap
+
+    // console.log(rollupConfig.dest.split('/').join(sep));
+
     bundle.write({
-      format: rollupConfig.format,
-      dest: rollupConfig.dest.split('/').join(sep)
+      dest: 'umd/app.umd.js',
+      moduleName: 'typescript.seed',
+      // output format - 'amd', 'cjs', 'es', 'iife', 'umd'
+      format: 'umd',
+      sourceMap: true
     }).then(function() {
       console.log('done');
       done();
