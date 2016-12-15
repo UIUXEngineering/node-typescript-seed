e82de0e50a67822fd5a9e94aacb8ff2af9479b7b
Jerry Orta <jerryorta.dev@gmail.com>
2016-12-15 01:09:34 -0600

    style(tslint): fix code length

--- src/_samples/design_patterns_in_typescript/decorator/demo.ts
+++ src/_samples/design_patterns_in_typescript/decorator/demo.ts
@@ -3,7 +3,9 @@ namespace DecoratorPattern {
   export namespace Demo {
 
     export function show(): void {
-      var decorator1: DecoratorPattern.Decorator = new DecoratorPattern.ConcreteDecorator(1, new DecoratorPattern.ConcreteComponent('Comp1'));
+      var decorator1: DecoratorPattern.Decorator =
+        new DecoratorPattern.ConcreteDecorator(1,
+          new DecoratorPattern.ConcreteComponent('Comp1'));
 
       decorator1.operation();
     }
