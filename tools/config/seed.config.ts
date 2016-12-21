9803fbaa17aaed1970d19c730e00c83b9f95598a
Jerry Orta <jerryorta.dev@gmail.com>
2016-12-21 02:10:35 -0600

    fix(update): fix update.from.sibling task

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -259,15 +259,15 @@ export class SeedConfig {
   ];
 
   SEED_SIBLING_COPY: any = [
-    join('..', this.SEED_SIBLING_DIR, '**', '*'),
-    '!' + join('..', this.SEED_SIBLING_DIR, 'coverage'),
-    '!' + join('..', this.SEED_SIBLING_DIR, 'dist'),
-    '!' + join('..', this.SEED_SIBLING_DIR, 'node_modules'),
-    '!' + join('..', this.SEED_SIBLING_DIR, 'src'),
-    '!' + join('..', this.SEED_SIBLING_DIR, 'CHANGELOG.md'),
-    '!' + join('..', this.SEED_SIBLING_DIR, 'seed.gulpfile.ts'),
-    '!' + join('..', this.SEED_SIBLING_DIR, 'README.md'),
-    '!' + join('..', this.SEED_SIBLING_DIR, 'yarn.lock')
+    join(this.SEED_SIBLING_DIR, '**', '*'),
+    '!' + join(this.SEED_SIBLING_DIR, 'coverage'),
+    '!' + join(this.SEED_SIBLING_DIR, 'dist'),
+    '!' + join(this.SEED_SIBLING_DIR, 'node_modules'),
+    '!' + join(this.SEED_SIBLING_DIR, 'src'),
+    '!' + join(this.SEED_SIBLING_DIR, 'CHANGELOG.md'),
+    '!' + join(this.SEED_SIBLING_DIR, 'seed.gulpfile.ts'),
+    '!' + join(this.SEED_SIBLING_DIR, 'README.md'),
+    '!' + join(this.SEED_SIBLING_DIR, 'yarn.lock')
   ];
 
   /**
