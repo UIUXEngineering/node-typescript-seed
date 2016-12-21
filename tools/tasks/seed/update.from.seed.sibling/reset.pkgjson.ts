1d225ee2131d5ebfa8dd214af2316c9dfd193d84
Jerry Orta <jerryorta.dev@gmail.com>
2016-12-21 09:05:20 -0600

    fix(update): save properties when updating package.json

--- tools/tasks/seed/update.from.seed.sibling/reset.pkgjson.ts
+++ tools/tasks/seed/update.from.seed.sibling/reset.pkgjson.ts
@@ -6,11 +6,13 @@ import Config from '../../../config';
 export = (done: any) => {
   let pkgjson = JSONParse('package.json');
   let version = _.clone(pkgjson.version);
+  let description = _.clone(pkgjson.description);
 
-  pkgjson = _.merge(pkgjson, Config.PACKAGE_JSON);
-  pkgjson.version = version;
+  let newPkgjson = _.merge(pkgjson, Config.PACKAGE_JSON);
+  newPkgjson.version = version;
+  newPkgjson.description = description;
 
-  let pkjsonString = JSON.stringify(pkgjson, null, 2);
+  let pkjsonString = JSON.stringify(newPkgjson, null, 2);
 
   fs.writeFile('package.json', pkjsonString, (err) => {
     if (err) {
